2d10e4a67cd0a22c0aac46d5eb7e05a586b1b45a
Brock Smedley <2791467+BrockSmedley@users.noreply.github.com>
2019-03-06 20:17:04 -0800

    add bootstrap & basic layout

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const cardImgStyle = {
+  width: "50%",
+  paddingTop: "20px"
+}
+
 class App extends Component {
   render() {
     return (
@@ -9,17 +14,52 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            The easiest way to build <code>cycling things here</code> on blockchain.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm">
+              <div className="card">
+                <div className="center">
+                  <img src="/img/admin.svg" style={cardImgStyle} class="card-img-top" alt="..." />
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">Use Case 1</h5>
+                  <p class = "card-text" >do X for Y with this</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm">
+              <div className="card">
+                <div className="center">
+                  <img src="/img/browser.svg" style={cardImgStyle} class="card-img-top" alt="..." />
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">Use Case 2</h5>
+                  <p class = "card-text" >do X for Y with this</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm">
+              <div className="card">
+                <div className="center">
+                  <img src="/img/email.svg" style={cardImgStyle} class="card-img-top" alt="..." />
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">Use Case 3</h5>
+                  <p class = "card-text" >do X for Y with this</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <footer style={{background: "#6666"}}>
+          <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" 		    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 		    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+        </footer>
       </div>
     );
   }
